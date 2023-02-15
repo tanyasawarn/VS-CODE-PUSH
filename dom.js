1165b36433a81704d819be3eaeda63e4b699fa5d
tanysawarn <tanyasawarn.222@gmail.com>
2023-02-15 20:57:54 +0530

    changing font

---
+++ dom.js
@@ -0,0 +1,14 @@
+//var header=document.getElementById('main-header');
+//headerTitle.style.borderBottom='solid 3px #000';
+//var title=document.getElementById('main');
+//title.style.fontWeight="900";
+//var title1=document.getElementById('main');
+//title1.style.color="green";
+ var items=document.getElementsByClassName('list-group-item');
+ items[1].textContent='Hello';
+ items[2].style.backgrounColor= 'green';
+ for(var i=0;i<items.length;i++)
+ {
+    items[i].style.fontWeight= 'bold';
+    items[i].style.fontColor='red';
+ }
