bb1ff5b80d3bc4b47d9b191f031ca3aa850c61ac
tanysawarn <tanyasawarn.222@gmail.com>
2023-02-16 11:44:43 +0530

    getelementbyid push

--- dom.js
+++ dom.js
@@ -4,11 +4,16 @@
 //title.style.fontWeight="900";
 //var title1=document.getElementById('main');
 //title1.style.color="green";
- var items=document.getElementsByClassName('list-group-item');
- items[1].textContent='Hello';
- items[2].style.backgrounColor= 'green';
- for(var i=0;i<items.length;i++)
- {
-    items[i].style.fontWeight= 'bold';
-    items[i].style.fontColor='red';
- }
+ //var items=document.getElementsByClassName('list-group-item');
+ //items[1].textContent='Hello';
+ //items[2].style.backgrounColor= 'green';
+ //for(var i=0;i<items.length;i++)
+ //{
+   // items[i].style.fontWeight= 'bold';
+    //items[i].style.fontColor='red';
+ //}
+ var li=document.getElementsByTagName('li');
+ li[4].style.backgroundColor= 'yellow';
+
+ var li=document.getElementsByClassName('list-group-item');
+ li[4].style.fontWeight= 'bold';
